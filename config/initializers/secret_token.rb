0c2812b4aa77cf037e4dd5addcc9f3ca8f6fc030
Oguz Serdar <oguz@limk.com>
2011-09-05 11:27:36 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Limkdeneme::Application.config.secret_token = '08d36eccbb3868e846e38b4acd68c748d25d92e509ebfa39f013df4fc7a3bfd7f55d706f6bec125b61036f168837f34266be53ffb9c73ac72b3ae4136b4c0674'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Limkdeneme::Application.config.session_store :cookie_store, key: '_limkdeneme_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Limkdeneme::Application.config.session_store :active_record_store
